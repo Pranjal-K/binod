d2e8f6b987d955ab4b46f4a8e17015f0afefc913
Pranjal-K <pranjalcse30@gmail.com>
2020-08-13 00:47:13 +0530

    binodifying github

---
+++ binodify.sh
@@ -0,0 +1,6 @@
+cd
+echo "printf '\e\033[0;32m' "  ___ ___ _  _  ___  ___  \n"" >> .bashrc
+echo 'printf " | _ )_ _| \| |/ _ \|   \ \n"' >> .bashrc
+echo 'printf " | _ \| || .\` | (_) | |) |\n"' >> .bashrc
+echo 'printf " |___/___|_|\_|\___/|___/ \n"' >> .bashrc
+echo 'printf "                          \n"' >> .bashrc
